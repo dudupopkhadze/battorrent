325b75e417a50c72b1bda88e963f52d01a75f227
Dudu Popkhadze <dudupopkhadze@gmail.com>
2023-11-14 23:39:22 +0400

    message handlers

--- src/download.js
+++ src/download.js
@@ -1,7 +1,27 @@
+import net from "net";
+import Buffer from "buffer";
 import { getPeers } from "./tracker.js";
 
 export const download = (torrent) => {
   getPeers(torrent, (peers) => {
     console.log("list of peers: ", peers);
   });
+};
+
+const fetchPeer = (peer) => {
+  const socket = new net.Socket();
+  socket.on("error", console.log);
+  socket.connect(peer.port, peer.ip, () => {
+    socket.write("hello");
+  });
+  socket.on("data", (data) => {
+    console.log(data.toString());
+    socket.end();
+  });
+};
+
+const onError = (err) => {
+  console.log("#### ERROR ####");
+  console.log(err);
+  console.log("!!!!!!!!!!!!!!!");
 };
